51ddce7f786121f037aa1ef107829499bfd33d45
Sam Mortimer <sam@mortimer.me.uk>
2018-08-20 20:56:38 -0700

    [DNM]: Exclude sepolicy for now
    Change-Id: I978d00e77cd5bb690892f62f6622ed16fb839dde

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_OTA_ASSERT_DEVICE := chiron
 AUDIO_FEATURE_ELLIPTIC_ULTRASOUND_SUPPORT := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from proprietary files
 include vendor/xiaomi/chiron/BoardConfigVendor.mk
